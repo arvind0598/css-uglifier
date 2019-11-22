5b2dfa419b299758fd652244e27cfde684d4d821
Arvind S <arvind0598@gmail.com>
2019-11-22 10:57:59 +0530

    test(extract-all-classes): fixed tests to expect a sorted array instead of records

--- test/constants/index.ts
+++ test/constants/index.ts
@@ -1,8 +1,22 @@
-export const BASIC_HTML_CLASSES = {
-  outer: 1,
-  inner1: 1,
-  innermost: 2,
-  inner2: 1,
-};
+import { ClassCount } from '../../src/types';
+
+export const BASIC_HTML_CLASS_LIST: Array<ClassCount> = [
+  {
+    className: 'innermost',
+    count: 2,
+  },
+  {
+    className: 'inner1',
+    count: 1,
+  },
+  {
+    className: 'inner2',
+    count: 1,
+  },
+  {
+    className: 'outer',
+    count: 1,
+  },
+];
 
 export const MORE_HTML = {};
--- test/extractAllClasses.spec.ts
+++ test/extractAllClasses.spec.ts
@@ -2,7 +2,7 @@
 import extractAllClasses from '../src/utils/extractAllClasses';
 import extractAllTags from '../src/utils/extractAllTags';
 import { readHTMLFile } from '../src/utils/readFile';
-import { BASIC_HTML_CLASSES } from './constants';
+import { BASIC_HTML_CLASS_LIST } from './constants';
 
 describe('extracting classes from basic html', () => {
   const document = readHTMLFile('basic');
@@ -10,6 +10,6 @@ describe('extracting classes from basic html', () => {
   const result = extractAllClasses(tags);
 
   test('extracted classes are accurate', () => {
-    expect(result).toStrictEqual(BASIC_HTML_CLASSES);
+    expect(result).toStrictEqual(BASIC_HTML_CLASS_LIST);
   });
 });
