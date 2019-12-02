70c1818c905b0f3dfdbec59d436bbe86c3ddae11
Arvind S <arvind0598@gmail.com>
2019-12-03 01:25:34 +0530

    docs(readfile): added jsdoc

--- src/utils/readFile.ts
+++ src/utils/readFile.ts
@@ -2,6 +2,21 @@ import fs from 'fs';
 import path from 'path';
 import { FileNotFoundError, MalformedFileError } from '../classes';
 
+/**
+ * @summary parses html to generate a Document
+ *
+ * @description
+ * parses html content via DOMParser
+ * the error handling is still not reliable for malformedfileerror
+ * online examples only exist for XML documents and/or browser environments
+ *
+ * @throws {MalformedFileError} error during parsing
+ * @throws {FileNotFoundError} file does not exist on disk
+ *
+ * @param {string} title of the file, without extension
+ *
+ * @returns {Document} if the document is succesfully parsed
+ */
 export const readHTMLFile = (title: string): Document | Error => {
   const htmlPath = path.join(process.cwd(), 'test', 'html', `${title}.html`);
   try {
