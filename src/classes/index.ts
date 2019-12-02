743c89302c775f4dcdf1b01b94ffc596958309cc
Arvind S <arvind0598@gmail.com>
2019-12-03 00:57:28 +0530

    fix(classes): explicitly set prototypes for custom errors
    ts-jest does not detect prototypes unless explicitly specified when checking type of thrown error

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,14 +1,19 @@
 /* eslint-disable max-classes-per-file */
+
+/* see https://github.com/facebook/jest/issues/8279 for setting prototype */
+
 export class FileNotFoundError extends Error {
   constructor(message: string) {
     super(message);
     this.name = 'ERROR__FILE_NOT_FOUND';
+    Object.setPrototypeOf(this, FileNotFoundError.prototype);
   }
 }
 
 export class MalformedFileError extends Error {
-  constructor(message: string) {
+  constructor(message: string = 'There was an error in parsing the file.') {
     super(message);
     this.name = 'ERROR__MALFORMED_FILE';
+    Object.setPrototypeOf(this, MalformedFileError.prototype);
   }
 }
