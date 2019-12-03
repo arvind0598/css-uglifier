47199685794be8f783689124fcf95ad22c76d7f0
Arvind S <arvind0598@gmail.com>
2019-12-03 10:33:39 +0530

    test(multiple classes): added tests for html elements with multiple classes added

--- test/extractAllClasses.spec.ts
+++ test/extractAllClasses.spec.ts
@@ -2,7 +2,7 @@
 import extractAllClasses from '../src/utils/extractAllClasses';
 import extractAllTags from '../src/utils/extractAllTags';
 import { readHTMLFile } from '../src/utils/readFile';
-import { BASIC_HTML_CLASS_LIST } from './constants';
+import { BASIC_HTML_CLASS_LIST, MULTIPLE_CLASSES_CLASS_LIST } from './constants';
 
 describe('extracting classes from basic html', () => {
   const document = readHTMLFile('basic');
@@ -11,5 +11,15 @@ describe('extracting classes from basic html', () => {
 
   test('extracted classes are accurate', () => {
     expect(result).toStrictEqual(BASIC_HTML_CLASS_LIST);
+  });
+});
+
+describe('extracting classes from multiple classes html', () => {
+  const document = readHTMLFile('multiple-classes');
+  const tags = extractAllTags(document);
+  const result = extractAllClasses(tags);
+
+  test('extracted classes are accurate', () => {
+    expect(result).toStrictEqual(MULTIPLE_CLASSES_CLASS_LIST);
   });
 });
