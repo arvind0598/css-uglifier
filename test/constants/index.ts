30cb9f790fd2db6f8ea1e749da4851da706820a7
Arvind S <arvind0598@gmail.com>
2019-12-03 10:32:40 +0530

    test(remapclasses): corrected the tests, does not use class map anymore

--- test/constants/index.ts
+++ test/constants/index.ts
@@ -1,8 +1,15 @@
-import { ClassCount, ClassMap } from '../../src/types';
+import { ClassCount } from '../../src/types';
 import BASIC_DATA from '../data/BASIC_DATA.json';
+import MULTIPLE_CLASS_DATA from '../data/MULTIPLE_CLASS_DATA.json';
 
-export const BASIC_HTML_CLASS_LIST: Array<ClassCount> = BASIC_DATA.PRETTY_CLASS_LIST;
+export const BASIC_HTML_CLASS_LIST:
+Array<ClassCount> = BASIC_DATA.PRETTY_CLASS_LIST;
 
-export const BASIC_HTML_CLASS_MAP: Array<ClassMap> = BASIC_DATA.UGLY_CLASS_LIST;
+export const BASIC_HTML_CLASS_MAP:
+Record<string, string> = BASIC_DATA.UGLY_CLASS_LIST;
 
-export const MORE_HTML = {};
+export const MULTIPLE_CLASSES_CLASS_LIST:
+Array<ClassCount> = MULTIPLE_CLASS_DATA.PRETTY_CLASS_LIST;
+
+export const MULTIPLE_CLASSES_CLASS_MAP:
+Record<string, string> = MULTIPLE_CLASS_DATA.UGLY_CLASS_LIST;
--- test/remapClasses.spec.ts
+++ test/remapClasses.spec.ts
@@ -1,17 +1,27 @@
-
 import extractAllClasses from '../src/utils/extractAllClasses';
 import extractAllTags from '../src/utils/extractAllTags';
 import remapClasses from '../src/utils/remapClasses';
 import { readHTMLFile } from '../src/utils/readFile';
-import { BASIC_HTML_CLASS_MAP } from './constants';
+import { BASIC_HTML_CLASS_MAP, MULTIPLE_CLASSES_CLASS_MAP } from './constants';
 
-describe('extracting classes from basic html', () => {
+describe('remapping classes from basic html', () => {
   const document = readHTMLFile('basic');
   const tags = extractAllTags(document);
   const classes = extractAllClasses(tags);
   const result = remapClasses(classes);
 
-  test('extracted classes are accurate', () => {
+  test('mapped classes are accurate', () => {
     expect(result).toStrictEqual(BASIC_HTML_CLASS_MAP);
+  });
+});
+
+describe('remapping classes from multiple classes html', () => {
+  const document = readHTMLFile('multiple-classes');
+  const tags = extractAllTags(document);
+  const classes = extractAllClasses(tags);
+  const result = remapClasses(classes);
+
+  test('mapped classes are accurate', () => {
+    expect(result).toStrictEqual(MULTIPLE_CLASSES_CLASS_MAP);
   });
 });
